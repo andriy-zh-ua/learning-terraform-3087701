35f82f6cfe87bfe962f4af340d07d844752a9705
andriy-zh-ua <semenyuknataliya@gmail.com>
2026-02-16 14:36:07 -0500

    Add associate_public_ip_address

--- main.tf
+++ main.tf
@@ -37,7 +37,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog-sg.security_group_id]
+  vpc_security_group_ids      = [module.blog-sg.security_group_id]
+  associate_public_ip_address = true
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
