dcdfa57f21a9056586a3bb1216eb53e7baa47efe
andriy-zh-ua <semenyuknataliya@gmail.com>
2026-02-16 13:51:41 -0500

    Use module for security group

--- main.tf
+++ main.tf
@@ -23,11 +23,26 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id] # Link the security group to the instance
+  vpc_security_group_ids = [module.security-group-blog-sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+# Security group for blog, where HTTP and HTTPS are allowed
+module "security-group-blog-sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+  version     = "5.3.1"
+  name        = "new-blog-sg"
+  description = "Security group for blog, where HTTP and HTTPS are allowed"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 # Security group for blog, where HTTP and HTTPS are allowed
