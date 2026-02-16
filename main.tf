102097f93200160dce3b40c4c0aff73d88914c3c
andriy-zh-ua <semenyuknataliya@gmail.com>
2026-02-16 15:39:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "blog_autoscaling" {
   instance_type        = var.instance_type
   image_id             = data.aws_ami.app_ami.id
 
-  traffic_source_attachment = {
+  traffic_source_attachments = {
     blog_alb = {
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
